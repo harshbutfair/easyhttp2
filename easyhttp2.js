7df60973bbe45f913526c097dff78c0e92e8bc98
harshbutfair <harsh.narayan@yahoo.com>
2021-06-30 04:07:46 +0530

    Added GET and POST request (Fetch with promise)

---
+++ easyhttp2.js
@@ -0,0 +1,28 @@
+class EasyHTTP {
+    // Make HTTP GET request
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        })
+
+    }
+
+    // Make an HTTP POST Request
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type' : 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        })
+    }
+}
